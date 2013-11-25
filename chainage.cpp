ea7510ca3a1f2b4cb85c105df2043208ac6c8a57
SacreDeBirmanie <sebful44@hotmail.fr>
2013-11-25 10:50:26 +0100

    contient classe chaine et classe Maillon

---
+++ chainage.cpp
@@ -0,0 +1,12 @@
+class Maillon<class T>{
+        private:
+           T element;
+           Maillon * suivant;
+          
+}
+
+class Chaine {
+        private:
+          int nbElements;
+          Maillon * tete;
+}
