ccd01f589f1e3fa8a3f71b72229ad18e51257ff7
SacreDeBirmanie <sebful44@hotmail.fr>
2013-11-26 12:07:07 +0100

    remplissage class chaine

--- chainage.cpp
+++ chainage.cpp
@@ -1,26 +1,31 @@
-class Maillon<class T>{
+template <class T>Maillon{
         private:
            T element;
-           Maillon * suivant;
+           Maillon<T> * suivant;
         public:
             void Maillon(T,Maillon*);
             ~Maillon(void);
 
 }
 
-class Chaine {
+void Maillon<T>::Maillon(T element, Maillon * maillon){
+    this->element=element;
+    this->suivant=maillon;
+}
+
+template <class T> Chaine {
         private:
 
           int nbElements;
-          Maillon * tete;
+          Maillon<T> &tete;
 
         public:
             ~Chaine(void);
             void initialiserChainage() : //initialise un chaˆınage;
-            bool estVide(...) : //teste si un chaˆınage est vide, i.e., ne contenant aucun ´el´ement ;
-            void reinitialiser(...) : //r´einitialise un chaˆınage en retirant tous ses ´el´ements ;
+            bool estVide() : //teste si un chaˆınage est vide, i.e., ne contenant aucun ´el´ement ;
+            void reinitialiser() : //r´einitialise un chaˆınage en retirant tous ses ´el´ements ;
             void insererTete(...) : //ins`ere un ´el´ement en tˆete du chaˆınage;
-            void insererQueue(...) : //ins`ere un ´el´ement en queue du chaˆınage;
+            void insererQueue(T) : //ins`ere un ´el´ement en queue du chaˆınage;
             void retirerTete(...) : //retire l’´el´ement de tˆete d’un chaˆınage;
             void retirerQueue(...) : //retire l’´el´ement de queue d’un chaˆınage;
             void insererOrdre(...) : //ins`ere un ´el´ement dans un chaˆınage dans l’ordre donn´e par une fonction de comparaison de type CompReleveMeteo (pass´ee en param`etre);
@@ -34,7 +39,7 @@ void initialiserChainage(){
     this->nbElements=0;
     this->tete=0;
 }
-bool estVide(...){
+bool estVide(){
     if(this->nbElements)
         return true;
     else
@@ -54,17 +59,48 @@ void reinitialiser(){
     }
 }
 
-void insererTete(){
+void insererTete(T element){
+       this->tete=new Maillon(element,this->tete);
+       this->nbElements=this->nbElements+1;
 
 }
-void insererQueue(...){
+void insererQueue(T element){
+    Maillon<T> mama=new Maillon(element,0);
+
+    if(this->tete==0){
+        this->tete=&mama;
+    }
+    else{
+        Maillon *p = this->tete;
+        while(!(p->suivant==0){
+            p=p->suivant;
+        }
+
+        this->nbElements = this->nbElements+1;
+        p->suivant=&mama;
+    }
+    this->nbElements=this->nbElements+1;
 
 }
-void retirerTete(...){
+void retirerTete(){
+    if(!(this->tete==0){
+        ptrTete=this->tete;
+        this->tete=this->tete.suivant;
+        free(ptrTete);
+        this->nbElements=this->nbElements-1;
+    }
 
 }
-void retirerQueue(...){
-
+void retirerQueue(){
+    if(!(this->tete==0) && !(this->tete->suivant==0)){
+        Maillon *p = this->tete;
+        while(!(p->suivant->suivant==0)){
+            p=p->suivant;
+        }
+        free(p->suivant);
+        p->suivant=0;
+        this->nbElements=this->nbElements-1;
+    }
 }
 void insererOrdre(...){
 
